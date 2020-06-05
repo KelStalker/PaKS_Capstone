93007d07d7fbc6bb46e7224a5d49276cdc824a57
Kel <kelstalker@hotmail.com>
2020-06-05 10:03:19 -0600

    Have defines the three main tables in the Trip Model

---
+++ PaKS_Backend/Models/Trip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaKS_Backend.Models
+{
+    public class TripTable
+    {
+        public Guid TripID { get; set; }
+        [Required]
+        public string Destination { get; set; }
+        public DateTime DepartDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+    }
+
+    public class PersonTable
+    {
+        public Guid PersonID { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int TelephoneNumber { get; set; }
+
+    }
+
+    public class ItemTable
+    {
+        public Guid ItemID { get; set; }
+        public string ItemName { get; set; }
+        public int ItemQuantity
+
+    }
+}
